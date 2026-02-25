68b51cf5f1cafe634f16884927b726c678ad0211
Mikhail Naganov <mnaganov@google.com>
2026-02-24 19:06:50 -0800

    Added field case 'build-2'

---
+++ test/build-2-trace.el
@@ -0,0 +1,167 @@
+;; Trace started at Tue Feb 24 19:01:43 2026
+(setq comint-9term-height-override 105)
+(setq width 76)
+;; Chunk len=3
+"12
+"
+;; Chunk len=200
+"ignoring XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX, not in /usr/local/google/home/mnaganov/code/master/vendor/google/build/allowed-vendorsetup_sh-files
+including vendor/google/build/vendorsetup.sh
+"
+;; Chunk len=370
+"Edit logs in the source tree are collected automatically, see go/edit_monitor_data_collect_policy for details and opt out instructions.
+including vendor/google/misc/metrics_uploader_prebuilt/vendorsetup.sh
+including vendor/google/tools/atest/vendorsetup.sh
+including vendor/google/tools/vendorsetup.sh
+including vendor/unbundled_google/tools/gemini-utils/vendorsetup.sh
+"
+;; Chunk len=1
+"
+"
+;; Chunk len=1217
+"============================================
+PLATFORM_VERSION_CODENAME=Baklava
+PLATFORM_VERSION=Baklava
+TARGET_PRODUCT=tokay
+TARGET_BUILD_VARIANT=userdebug
+TARGET_ARCH=arm64
+TARGET_ARCH_VARIANT=armv8-7a
+TARGET_CPU_VARIANT=cortex-a55
+HOST_OS=linux
+HOST_OS_EXTRA=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+HOST_CROSS_OS=windows
+BUILD_ID=MAIN
+OUT_DIR=out
+SOONG_PARTIAL_COMPILE=true
+SOONG_NINJA=siso
+USE_RBE=true
+USE_REWRAPPER=true
+SOONG_ONLY=true
+SOONG_INCREMENTAL_ANALYSIS=false
+============================================
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Wondering whether to use user, userdebug or eng?
+
+  user        The builds that ship to users. Reduced debugability.
+  userdebug   High fidelity to user builds but with some debugging options
+              enabled. Best suited for performance testing or day-to-day use
+              with debugging enabled.
+  eng         More debugging options enabled and faster build times, but
+              runtime performance tradeoffs. Best suited for day-to-day
+              local development when not doing performance testing.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"
+;; Chunk len=200
+"ignoring XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX, not in /usr/local/google/home/mnaganov/code/master/vendor/google/build/allowed-vendorsetup_sh-files
+including vendor/google/build/vendorsetup.sh
+"
+;; Chunk len=370
+"Edit logs in the source tree are collected automatically, see go/edit_monitor_data_collect_policy for details and opt out instructions.
+including vendor/google/misc/metrics_uploader_prebuilt/vendorsetup.sh
+including vendor/google/tools/atest/vendorsetup.sh
+including vendor/google/tools/vendorsetup.sh
+including vendor/unbundled_google/tools/gemini-utils/vendorsetup.sh
+"
+;; Chunk len=18
+"[?25l[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=68
+"[1mStarting CIPD proxy server...[0m[K[1mStarting rbe...[0m[K"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=6
+"[1;1r"
+;; Chunk len=50
+"[1;1H[1mRunning product configuration...[0m[K"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=542
+"[K============================================
+PLATFORM_VERSION_CODENAME=Baklava
+PLATFORM_VERSION=Baklava
+TARGET_PRODUCT=tokay
+TARGET_BUILD_VARIANT=userdebug
+TARGET_ARCH=arm64
+TARGET_ARCH_VARIANT=armv8-7a
+TARGET_CPU_VARIANT=cortex-a55
+HOST_OS=linux
+HOST_OS_EXTRA=Linux-6.16.12-1rodete2-amd64-x86_64-Debian-GNU/Linux-rodete
+HOST_CROSS_OS=windows
+BUILD_ID=MAIN
+OUT_DIR=out
+SOONG_PARTIAL_COMPILE=true
+SOONG_NINJA=siso
+USE_RBE=true
+USE_REWRAPPER=true
+SOONG_ONLY=true
+SOONG_INCREMENTAL_ANALYSIS=true
+============================================
+"
+;; Chunk len=42
+"[1m[  0% 0/1] bootstrap blueprint[0m[K"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=42
+"[1m[100% 1/1] bootstrap blueprint[0m[K"
+;; Chunk len=28
+"[1mRunning globs...[0m[K"
+;; Chunk len=19
+"[1mninja: [0m[K"
+;; Chunk len=71
+"[1mninja: [33mSISO_EXPERIMENTS=fallback-on-exec-error enabled[0m[K"
+;; Chunk len=55
+"[1mninja: ignore missing out error in depfile [0m[K"
+;; Chunk len=106
+"[1m[ 50% 1/2] analyzing Android.bp files and generating ninja file at out/soong/build.tokay.ninja[0m[K"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=12
+"[1;1r[1;1H"
+;; Chunk len=92
+"[K19:02:18 Got signal: interrupt
+19:02:18 cipd: unexpected EOF, process exited with <nil>
+"
+;; Chunk len=171
+"19:02:19 soong bootstrap failed with: exit status 130
+19:02:19 failed to clean up cipd proxy socket XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.
+"
+;; Chunk len=58
+"[1;1r[1;1H[r[1;1H[?25h
+RBE Stats: down 0 B, up 0 B, 
+"
+;; Chunk len=54
+"
+#### failed to build some targets (22 seconds) ####
+
+"
