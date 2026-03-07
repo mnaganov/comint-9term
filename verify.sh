6c598424639ae3e3c0f1b4e9d6bb641ae64988c2
Mikhail Naganov <mikhail.naganov@gmail.com>
2026-03-06 19:29:12 -0800

    Update zsh-prompt test to properly reproduce the trace and fail on unpatched system

--- verify.sh
+++ verify.sh
@@ -21,9 +21,9 @@ trap restore_terminal EXIT
 # Configuration
 ERRORS_FILE="out/elisp-errors.txt"
 # Tests that simply compare output against a static file in test/
-SIMPLE_TESTS=("ansi-seq" "password-test" "ssh-output" "trace-test" "shell-reexec" "zsh-prompt")
+SIMPLE_TESTS=("ansi-seq" "password-test" "ssh-output" "trace-test" "shell-reexec")
 # Tests that require screen execution to generate a golden master for comparison
-SCREEN_TESTS=("apt-prog" "apt-prog-0" "apt-prog-30" "apt-prog-70" "build" "compile" "window-height")
+SCREEN_TESTS=("apt-prog" "apt-prog-0" "apt-prog-30" "apt-prog-70" "build" "compile" "window-height" "zsh-prompt")
 # All tests combined
 ALL_TESTS=("${SIMPLE_TESTS[@]}" "${SCREEN_TESTS[@]}")
 
